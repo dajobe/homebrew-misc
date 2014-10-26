37a330c2cc12f9ef78b580942fc68c1bb960be64
Dave Beckett <dave@dajobe.org>
2014-10-26 16:04:50 -0700

    fix

--- cargo.rb
+++ cargo.rb
@@ -1,15 +1,15 @@
 require "formula"
 
 class Cargo < Formula
-  homepage "http://crates.io"
-  url "curl -O https://static.rust-lang.org/cargo-dist/cargo-nightly-x86_64-apple-darwin.tar.gz"
+  homepage "http://crates.io/"
+  url "https://static.rust-lang.org/cargo-dist/cargo-nightly-x86_64-apple-darwin.tar.gz"
   version "nightly"
   head "https://github.com/rust-lang/cargo.git"
 
   depends_on "rust"
 
   def install
-    system "make", "PREFIX=#{prefix}"
-    system "make", "PREFIX=#{prefix}", "install"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
   end
 end
